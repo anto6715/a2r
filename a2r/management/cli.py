ab0e40fb6642ef7c4c8d487c19c10455b009c718
Antonio Mariani <antonio.mariani@cmcc.it>
2024-08-14 10:32:15 +0200

    review args pt2

--- a2r/management/cli.py
+++ a2r/management/cli.py
@@ -32,8 +32,8 @@ def rolling(config, dry_run, **kwargs):
 
 
 @main.command()
-@click.argument("--keep", "path_to_keep", type=Path, required=True)
-@click.argument("--clean", "path_to_clean", type=Path, required=True)
+@click.option("--keep", "path_to_keep", type=Path, required=True)
+@click.option("--clean", "path_to_clean", type=Path, required=True)
 @click.option(
     "-f",
     "--force",
@@ -54,7 +54,7 @@ def saferm(
 
 
 @main.command()
-@click.argument("-p", "--path", "path", type=Path, required=True)
+@click.option("-p", "--path", "path", type=Path, required=True)
 @verbose_option
 def update_md5(path: Path, **kwargs):
     settings.configure(**kwargs)
